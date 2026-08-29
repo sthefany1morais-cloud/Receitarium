0575ee0917f530e2089ec2e92d41586516cdff4b
Stefany Maria <sthefan1morais@gmail.com>
2026-08-29 14:29:06 -0300

    first commit

---
+++ src/main/java/com/receitarium/receitarium/ReceitariumApplication.java
@@ -0,0 +1,13 @@
+package com.receitarium.receitarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitariumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/receitarium/receitarium/ReceitariumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receitarium.receitarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitariumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
